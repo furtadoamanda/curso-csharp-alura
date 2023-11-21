c5649f360fd10f4f738083c9dcbae4d60791f549
Amanda Furtado <furtadodesouzaamanda@gmail.com>
2023-11-21 20:48:28 -0300

    Aula 5

--- C# Alura/Program.cs
+++ C# Alura/Program.cs
@@ -41,10 +41,9 @@ void ExibirOpcoesDoMenu()
             break;
         case 2: MostrarBandasRegistradas();
             break;
-        case 3:AvaliarUmaBanda();
+        case 3: AvaliarUmaBanda();
             break;
-        case 4:
-            Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
+        case 4: ExibirMedia();
             break;
         case -1:
             Console.WriteLine("Tchau tchau :)");
@@ -115,4 +114,29 @@ void AvaliarUmaBanda()
     }
 }
 
+void ExibirMedia()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir média da banda");
+    Console.Write("Digite o nome da banda que deseja exibir a média: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        List<int> notasDaBanda = bandasRegistradas[nomeDaBanda];
+        Console.WriteLine($"\nA média da banda {nomeDaBanda} é {notasDaBanda.Average()}");
+        Console.WriteLine("Digite uma tecla para voltar ao menu.");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    } else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada");
+        Console.WriteLine("Digite uma tecla para voltar ao menu.");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
 ExibirOpcoesDoMenu();
+
