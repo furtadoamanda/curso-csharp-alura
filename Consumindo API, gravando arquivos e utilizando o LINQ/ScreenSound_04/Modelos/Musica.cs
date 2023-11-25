a71f11a54cea72fce19cf1b7052ca845f2dda223
Amanda Furtado <furtadodesouzaamanda@gmail.com>
2023-11-25 12:19:08 -0300

    Curso 4

---
+++ Consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound_04/Modelos/Musica.cs
@@ -0,0 +1,51 @@
+namespace ScreenSound_04.Modelos;
+
+internal class Musica
+{
+    private string[] tonalidades = {"C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"};
+
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    [JsonPropertyName("year")]
+    public string? AnoString { get; set; }
+
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+    public string Tonalidade 
+    {
+        get
+            {
+                return tonalidades[Key];
+            }
+    }
+
+
+    public int Ano
+    {
+        get
+        {
+            return int.Parse(AnoString!);
+        }
+    }
+
+
+    public void ExibirDeatlhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Gênero musical: {Genero}");
+        Console.WriteLine($"Ano: {Ano}");
+        Console.WriteLine($"Tonalidade: {Tonalidade}");
+    }
+}
